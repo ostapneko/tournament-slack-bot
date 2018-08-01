edae10e0231a1c76c8783ed19dcb59da8592a3d6
Thomas Franquelin <ostapneko@gmail.com>
2018-08-01 22:35:26 +0200

    Add a show command

--- app/controllers/tournament_controller.rb
+++ app/controllers/tournament_controller.rb
@@ -3,10 +3,13 @@ class TournamentController < ApplicationController
     case params['text']
     when 'help'
       answer_text <<-EOM
-create <tournament-name>
+create <tournament-name>: create a new tournament
+show: show all the tournaments
       EOM
     when /^create (\S+)$/
       create_tournament $1
+    when /^show$/
+      answer_text get_tournament_names
     else
       answer_text "What is this command?"
     end
@@ -25,5 +28,9 @@ create <tournament-name>
       else
         answer_text "Invalid creation: #{t.errors.full_messages.join(', ')}"
       end
+  end
+
+  def get_tournament_names
+    Tournament.select(:name).map(&:name).join("\n")
   end
 end
